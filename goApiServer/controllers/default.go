f5d43847959a6ab025c7651be6abc9bb108ea00d
willjiang <jlh19900430@163.com>
2019-07-12 23:46:02 +0800

    import mysql & add provider model

--- goApiServer/controllers/default.go
+++ goApiServer/controllers/default.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/astaxie/beego"
 )
 
@@ -9,6 +10,14 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
+	pa := c.Ctx.Input.Param(":pa")
+	userName := c.Ctx.Input.Query("userName")
+	passWord := c.Ctx.Input.Query("password")
+	fmt.Printf("paramsList is ===>  pa: %s name: %s  pwd: %s" , string(pa), string(userName), string(passWord))
+
+
+	fmt.Println()
+	fmt.Println("======================================================================================")
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
 	c.TplName = "index.tpl"
--- goApiServer/main.go
+++ goApiServer/main.go
@@ -1,11 +1,47 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	_ "myGoWeb/goApiServer/routers"
 	"github.com/astaxie/beego"
+	"time"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
+	// init to connect DB
+	db, _ := connectDB()
+	_, err := db.Exec("insert into users (username, password) values(?, ?)", "will", "zte")
+	if err != nil {
+		fmt.Println("insert into database failed with errMsg ==> " + err.Error())
+	}
+
+	// init provider
+
+	// init model
+
+
+	startBeegoWebServer()
+}
+
+func connectDB() (*sql.DB, error) {
+	var db *sql.DB
+	var err error
+	connectString := "willjiang:willjiang@tcp(localhost:3306)/angular?charset=utf8"
+	for {
+		db, err = sql.Open("mysql", connectString)
+		if err != nil {
+			fmt.Println("Connect DB mysql failed , trying after 5 second. errMsg ==> " + err.Error())
+			time.Sleep( 5 * time.Second)
+			continue
+		}
+		break
+	}
+	return db, nil
+}
+
+func startBeegoWebServer() {
 	beego.Run()
 }
 
